562de3cf1cf00d6c6fa96696b68c8a638b789310
Nanna <nanna@e-nanna.dk>
2016-08-23 15:55:15 +0200

    Updated files

--- js/ku-menus.js
+++ js/ku-menus.js
@@ -7,29 +7,29 @@
 +function ($) {
     'use strict';
 
-    var $kuNav = $('#kunav');
-    var $navBar = $('#navbar');
+    // var $kuNav = $('#kunav');
+    // var $navBar = $('#navbar');
     var $cachedWidth = $('body').prop('clientWidth');
     if ($cachedWidth <= 768) {
       // Global menu folding
-      $navBar.on('show.bs.collapse', function () {
-        $kuNav.show();
-      });
-      $navBar.on('hide.bs.collapse', function () {
-        $kuNav.hide();
-      });
+    //   $navBar.on('show.bs.collapse', function () {
+    //     $kuNav.show();
+    //   });
+    //   $navBar.on('hide.bs.collapse', function () {
+    //     $kuNav.hide();
+    //   });
 
       // Clicking left menu button or content area closes global menu
-      $('#btn_left, #content').click(function () {
-        $('#navbar').collapse('hide');
-      });
+    //   $('#btn_left, #content').click(function () {
+    //     $navBar.collapse('hide');
+    //   });
     }
 
     $(window).resize(function () {
       var $newWidth = $('body').prop('clientWidth');
       if ($newWidth !== $cachedWidth) {
-        $kuNav.removeAttr('style'); // reset style
-        $('#btn_right').addClass('collapsed'); // reset burger icon
+        //$kuNav.removeAttr('style'); // reset style
+        //$('#btn_right').addClass('collapsed'); // reset burger icon
         $cachedWidth = $newWidth;
       }
     });
