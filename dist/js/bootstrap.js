c382c5086b6b998acf3dad5bb2465ee25abc0ac7
Nanna <nanna@e-nanna.dk>
2016-08-24 15:09:10 +0200

    Updated files

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -53,7 +53,6 @@ if (typeof jQuery === 'undefined') {
   });
 }(jQuery);
 
-// jscs:disable
 /* ======================================================================
  * Bootstrap: v3.3.7
  * Custom menu script for University of Copenhagen: ku.dk
@@ -62,30 +61,14 @@ if (typeof jQuery === 'undefined') {
 + function ($) {
   'use strict';
 
-  // var $kuNav = $('#kunav');
-  // var $navBar = $('#navbar');
   var $cachedWidth = $('body').prop('clientWidth');
-  if ($cachedWidth <= 768) {
-    // Global menu folding
-    //   $navBar.on('show.bs.collapse', function () {
-    //     $kuNav.show();
-    //   });
-    //   $navBar.on('hide.bs.collapse', function () {
-    //     $kuNav.hide();
-    //   });
-
-    // Clicking left menu button or content area closes global menu
-    //   $('#btn_left, #content').click(function () {
-    //     $navBar.collapse('hide');
-    //   });
-  }
 
   $(window).resize(function () {
     var $newWidth = $('body').prop('clientWidth');
     if ($newWidth !== $cachedWidth) {
-      if ($cachedWidth <= 768) {
-        $('#mobileleftmenu').remove(); // Remove mobile menu
-      }
+        // Close mobile menu on resize
+      var mmenu = $('#mobileleftmenu').mmenu().data("mmenu");
+      mmenu.close();
       $cachedWidth = $newWidth;
     }
   });
--- js/ku-menus.js
+++ js/ku-menus.js
@@ -1,4 +1,3 @@
-// jscs:disable
 /* ======================================================================
  * Bootstrap: v3.3.7
  * Custom menu script for University of Copenhagen: ku.dk
@@ -7,30 +6,14 @@
 + function ($) {
   'use strict';
 
-  // var $kuNav = $('#kunav');
-  // var $navBar = $('#navbar');
   var $cachedWidth = $('body').prop('clientWidth');
-  if ($cachedWidth <= 768) {
-    // Global menu folding
-    //   $navBar.on('show.bs.collapse', function () {
-    //     $kuNav.show();
-    //   });
-    //   $navBar.on('hide.bs.collapse', function () {
-    //     $kuNav.hide();
-    //   });
-
-    // Clicking left menu button or content area closes global menu
-    //   $('#btn_left, #content').click(function () {
-    //     $navBar.collapse('hide');
-    //   });
-  }
 
   $(window).resize(function () {
     var $newWidth = $('body').prop('clientWidth');
     if ($newWidth !== $cachedWidth) {
-      if ($cachedWidth <= 768) {
-        $('#mobileleftmenu').remove(); // Remove mobile menu
-      }
+        // Close mobile menu on resize
+      var mmenu = $('#mobileleftmenu').mmenu().data("mmenu");
+      mmenu.close();
       $cachedWidth = $newWidth;
     }
   });
