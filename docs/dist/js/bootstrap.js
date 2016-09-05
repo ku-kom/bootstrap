f70ec2cf13dbab5c70e24db43c11fa24823386da
Nanna <nanna@e-nanna.dk>
2016-09-05 15:51:37 +0200

    Updated files2

--- docs/dist/js/bootstrap.js
+++ docs/dist/js/bootstrap.js
@@ -53,6 +53,27 @@ if (typeof jQuery === 'undefined') {
   });
 }(jQuery);
 
+/* ======================================================================
+ * Bootstrap: v3.3.7
+ * Custom menu script for University of Copenhagen: ku.dk
+ * ====================================================================== */
+
++function ($) {
+  'use strict';
+
+  var $cachedWidth = $('body').prop('clientWidth');
+
+  $(window).resize(function () {
+    var $newWidth = $('body').prop('clientWidth');
+    if ($newWidth !== $cachedWidth) {
+        // Close mobile menu on resize
+      var mmenu = $('#mobileleftmenu').mmenu().data('mmenu');
+      mmenu.close();
+      $cachedWidth = $newWidth;
+    }
+  });
+}(jQuery);
+
 /* ========================================================================
  * Bootstrap: transition.js v3.3.7
  * http://getbootstrap.com/javascript/#transitions
