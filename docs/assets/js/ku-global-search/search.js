ca909f4e4fa93eb7344a6e898581483ad263d331
Nanna Ellegaard <nanna@e-nanna.dk>
2017-03-13 15:52:15 +0100

    Updated files

--- docs/assets/js/ku-global-search/search.js
+++ docs/assets/js/ku-global-search/search.js
@@ -1,10 +1,10 @@
 window.searchCallbacks = [];
 
-jQuery(function(){
+jQuery(function() {
 
-  var stripPath = function(path){
-    return path === "/" ? path : path.replace(/\/$/,"");
-  }
+  var stripPath = function(path) {
+    return path === "/" ? path : path.replace(/\/$/, "");
+  };
 
   var isEngineCurrent = function(engine) {
     if (stripPath(engine.url) !== stripPath(document.location.origin + document.location.pathname)) {
@@ -19,41 +19,54 @@ jQuery(function(){
     }
     return true;
   };
-
+  $(document).click('click', 'li', function() {
+    var $area = $('#search');
+    $('li[data-selected="selected"]').attr('data-selected', '');
+    $(this).attr('data-selected', 'selected');
+    var $dropdownText = $(this).attr('data-selected', 'selected').text();
+    $area.text($dropdownText);
+    $area.append('<span class="caret"></span><span class="sr-only">Search options</span>');
+  });
   var forms = jQuery("form.search_form");
   forms.each(function() {
     var form = jQuery(this);
-    var field = form.find("input.search_query");
-    var filter = form.find("select.search_filter");
+    var field = form.find("#search_query");
+    var filter = form.find("#searchfilter");
     var resetForm = form.hasClass("search_reset");
 
     if (window.searchEngines) {
-      for (var i=0; i<window.searchEngines.length; i++) {
+      for (var i = 0; i < window.searchEngines.length; i++) {
         var engine = window.searchEngines[i];
-        var option = jQuery(document.createElement("option"));
+        var option = jQuery(document.createElement("li"));
         option.text(engine.label);
-        option.val(i);
+        option.attr('data-value', i);
+        if (i === 0) {
+          option.attr('data-selected', 'selected');
+        } else {
+          option.attr('data-selected', '');
+        }
         if (!resetForm && isEngineCurrent(engine)) {
-          option.attr("selected","selected");
+          option.attr("selected", "selected");
           field.val(getUrlParameter(engine.querykey));
         }
         filter.append(option);
       }
-      form.submit(function(event){
-        var chosenEngine = window.searchEngines[filter.val()];
+      form.submit(function(event) {
+        var chosenEngine = window.searchEngines[$('li[data-selected="selected"]').data('value')];
+        //console.log(chosenEngine);
         form.attr("action", chosenEngine.url);
         form.attr("method", chosenEngine.method || "GET");
         field.attr("name", chosenEngine.querykey);
         if (chosenEngine.param) {
           for (var paramName in chosenEngine.param) {
             var input = jQuery(document.createElement("input"));
-            input.attr("type","hidden");
-            input.attr("name",paramName);
+            input.attr("type", "hidden");
+            input.attr("name", paramName);
             input.val(chosenEngine.param[paramName]);
             form.append(input);
           }
         }
-        for (var i=0; i<window.searchCallbacks.length; i++) {
+        for (var i = 0; i < window.searchCallbacks.length; i++) {
           var callback = window.searchCallbacks[i];
           if (jQuery.isFunction(callback)) {
             callback(chosenEngine, this);
@@ -63,5 +76,3 @@ jQuery(function(){
     }
   });
 });
-
-
