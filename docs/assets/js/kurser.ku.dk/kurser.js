2c8e2810727a6b4da94f76b400a6b8722decf4ff
Nanna Ellegaard <nanna@e-nanna.dk>
2017-03-23 13:43:01 +0100

    Updated files

--- docs/assets/js/kurser.ku.dk/kurser.js
+++ docs/assets/js/kurser.ku.dk/kurser.js
@@ -1,14 +1,15 @@
 // Custom scripts for kurser.ku.dk
 $(document).ready(function() {
-  var $cachedWidth = $('body').prop('clientWidth');
-  var $search = $('#searchall');
-  var $hiddenColumn = $('.mobile-hidden');
+  $cachedWidth = $('body').prop('clientWidth');
+  $search = $('#searchall');
+  $advanced = $('#showall');
+  $hiddenColumn = $('.mobile-hidden');
 
   resetText();
   multipleSelectBox();
   runDatatable();
 
-  $('#showall').click(function() {
+  $advanced.click(function() {
     if ($cachedWidth < 768) {
       $(this).toggleClass('open');
       $hiddenColumn.slideToggle();
@@ -19,64 +20,66 @@ $(document).ready(function() {
     var $newWidth = $('body').prop('clientWidth');
     if ($newWidth !== $cachedWidth) {
       $hiddenColumn.removeAttr('style');
+      $advanced.removeClass('open');
       resetText();
       $cachedWidth = $newWidth;
     }
     $cachedWidth = $newWidth;
   });
-
-  // Function to change text for small screens
-  function resetText() {
-    var lang = $('html').attr('lang');
-
-    $('.form-group .btn').each(function() {
-      if ($cachedWidth < 768 && lang == 'da') {
-        $search.html('Søg');
-      } else {
-        $(this).html($(this).data(lang));
-      }
-    });
-  }
-
-  function runDatatable() {
-    // Datatable language switcher
-    function getLanguage() {
-      var langMap = {
-        en: 'English',
-        da: 'Danish'
-      };
-      var lang = $('html').attr('lang');
-      return '//cdn.datatables.net/plug-ins/1.10.13/i18n/' + langMap[lang] + '.json';
-    }
-    $('#searchresults').DataTable({
-      language: {
-        url: getLanguage()
-      },
-      ordering: true,
-      autoWidth: false,
-      bFilter: false,
-      fixedHeader: true,
-      responsive: true
-    });
-  }
-
-  function multipleSelectBox() {
-    // Multiple select language switcher
-    var multipleSelectLang = function() {
-      var da = {
-        placeholder: 'V&aelig;lg flere...',
-        selectAllText: 'V&aelig;lg alle',
-        allSelected: 'Alle valgt',
-        countSelected: '# af % valgt',
-        width: '100%'
-      };
-      var en = {
-        placeholder: 'Select multiple...',
-        width: '100%'
-      };
-      // Run Multiple select
-      return ($('html').attr('lang') == 'da') ? da : en;
-    };
-    $('select[multiple]').multipleSelect(multipleSelectLang());
-  }
 });
+
+// Function to change text for small screens (DA/ENG)
+function resetText() {
+  var $lang = $('html').attr('lang');
+  $('.form-group .btn').each(function() {
+    if ($cachedWidth < 768 && $lang == 'da') {
+      $search.html('Søg');
+    } else {
+      $(this).html($(this).data($lang));
+    }
+  });
+}
+
+function runDatatable() {
+  // Datatable language switcher
+  function getLanguage() {
+    var $langMap = {
+      en: 'English',
+      da: 'Danish'
+    };
+    var $lang = $('html').attr('lang');
+    return '//cdn.datatables.net/plug-ins/1.10.13/i18n/' + $langMap[$lang] + '.json';
+  }
+
+  // Run Datatables
+  $('#searchresults').DataTable({
+    language: {
+      url: getLanguage()
+    },
+    ordering: true,
+    autoWidth: false,
+    bFilter: false,
+    fixedHeader: true,
+    responsive: true
+  });
+}
+
+function multipleSelectBox() {
+  // Multiple select language switcher
+  var multipleSelectLang = function() {
+    var da = {
+      placeholder: 'V&aelig;lg flere...',
+      selectAllText: 'V&aelig;lg alle',
+      allSelected: 'Alle valgt',
+      countSelected: '# af % valgt',
+      width: '100%'
+    };
+    var en = {
+      placeholder: 'Select multiple...',
+      width: '100%'
+    };
+    // Run Multiple select
+    return ($('html').attr('lang') == 'da') ? da : en;
+  };
+  $('select[multiple]').multipleSelect(multipleSelectLang());
+}
