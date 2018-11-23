428928e2b1772f2091c7b405a0c9f95416c65d66
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2018-11-23 12:37:06 +0100

    Make search javascript work better with employee pages

--- docs/assets/js/ku-global-search/search.js
+++ docs/assets/js/ku-global-search/search.js
@@ -1,5 +1,5 @@
 window.searchCallbacks = [];
-jQuery(function() {
+jQuery(function(jQuery) {
   var forms = jQuery('.global-search'),
     current_engine = {};
   function initSearchForm() {
@@ -11,31 +11,32 @@ jQuery(function() {
       filter_btn.html(window.searchEngines[0].label);
       current_engine = window.searchEngines[0];
 
-      for (var se in window.searchEngines) {
-        var engine = window.searchEngines[se],
-          option = jQuery(document.createElement('li'));
+      jQuery.each(window.searchEngines, function(index, value) {
+        var engine = value,
+            option = jQuery(document.createElement('li'));
+
         option.text(engine.label);
         option.attr('data-label', engine.label);
-        option.attr('data-value', se);
+        option.attr('data-value', index);
         option.on('click', function(event) {
           filter_btn.html(event.target.dataset.label);
           current_engine = window.searchEngines[event.target.dataset.value];
         });
         filter.append(option);
-      }
+      });
 
-      form.submit(function(event) {
+      form.submit(function() {
         form.attr('action', current_engine.url);
         form.attr('method', current_engine.method || 'GET');
         field.attr('name', current_engine.querykey);
         if (current_engine.param) {
-          for (var paramName in current_engine.param) {
+          jQuery.each(current_engine.param, function(key, value) {
             var input = jQuery(document.createElement('input'));
             input.attr('type', 'hidden');
-            input.attr('name', paramName);
-            input.val(current_engine.param[paramName]);
+            input.attr('name', key);
+            input.val(value);
             form.append(input);
-          }
+          });
         }
         for (var i = 0; i < window.searchCallbacks.length; i++) {
           var callback = window.searchCallbacks[i];
@@ -45,12 +46,12 @@ jQuery(function() {
         }
       });
     });
-  };
+  }
 
   if (window.searchEngines) {
     initSearchForm();
   } else {
     forms.remove();
-    console.log('No search engine data available.')
-  };
+    console.log('No search engine data available.');
+  }
 });
