990e4258f60b2b4bf088d16d90788f68c7fe4f97
cdq217 <nel@adm.ku.dk>
2019-03-27 10:12:40 +0100

    Social media

--- docs/assets/js/social-feeds/instagram/instagramByAccount.js
+++ docs/assets/js/social-feeds/instagram/instagramByAccount.js
@@ -18,7 +18,7 @@
     var $loading = $wrapper.find(".ku-loading");
     var $token = $wrapper.attr("data-token");
     var $user = $wrapper.attr("data-account");
-    var $hidemobile = (typeof $wrapper.attr("data-hidemobile") == null) ? false : true;
+    var $hidemobile = (typeof $wrapper.attr("data-hidemobile") == null || false) ? false : true;
     var $accountName = (typeof $user === 'undefined') ? 'university_of_copenhagen' : $user.trim();
     var $batchClass = "batch";
     var $cachedWidth = $('body').prop('clientWidth');
