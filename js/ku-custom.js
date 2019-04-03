d007d8af636c7c3866aeb8c3b6890703620fa08f
cdq217 <nel@adm.ku.dk>
2019-04-03 10:40:14 +0200

    Spinner

--- js/ku-custom.js
+++ js/ku-custom.js
@@ -78,14 +78,12 @@ function shareURL(dest) {
     });
   }
 
-  // Function to make parent items in global menu clickable although they hold dropdown menus. Add class 'disabled' for desktop only:
+  // Function to make parent items in global menu clickable although they hold dropdown menus. Add class 'disabled':
   function makeGlobalMenuClickable() {
-    //if (window.matchMedia('(min-width: 768px)').matches) {
-      var $menu = $('#navbar_menu li.dropdown');
-      $menu.each(function () {
-        $(this).children('.dropdown-toggle').addClass('disabled');
-      });
-    //}
+    var $menu = $('#navbar_menu li.dropdown');
+    $menu.each(function () {
+      $(this).children('.dropdown-toggle').addClass('disabled');
+    });
   }
   makeGlobalMenuClickable();
 
