28fdedfe553bd544e5df86c9da62b3ffebd2062a
nanna-dk <nanna@e-nanna.dk>
2019-09-19 14:09:25 +0200

    Cookie js

--- docs/assets/js/cookie/cookie.js
+++ docs/assets/js/cookie/cookie.js
@@ -5,40 +5,36 @@
 // if (readCookie("some-name") === null) {
 //   Do something if it doesn't esist
 // }
-(function () {
-  'use strict';
+// Create cookie
+var createCookie = function (name, value, days) {
+  var expires;
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toGMTString();
+  } else {
+    expires = '';
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
 
-  // Create cookie
-  function createCookie(name, value, days) {
-    var expires;
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = '; expires=' + date.toGMTString();
-    } else {
-      expires = '';
+// Read cookie
+var readCookie = function (name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1, c.length);
     }
-    document.cookie = name + '=' + value + expires + '; path=/';
-  }
-
-  // Read cookie
-  function readCookie(name) {
-    var nameEQ = name + '=';
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1, c.length);
-      }
-      if (c.indexOf(nameEQ) === 0) {
-        return c.substring(nameEQ.length, c.length);
-      }
+    if (c.indexOf(nameEQ) === 0) {
+      return c.substring(nameEQ.length, c.length);
     }
-    return null;
   }
+  return null;
+}
 
-  // Erase cookie
-  function eraseCookie(name) {
-    createCookie(name, '', -1);
-  }
-})();
+// Erase cookie
+var eraseCookie = function (name) {
+  createCookie(name, '', -1);
+}
