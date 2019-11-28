57ab3c8a17904a08c33fa3bc98abc171e31b43e9
nanna-dk <nanna@e-nanna.dk>
2019-11-28 14:47:17 +0100

    Video player taborder

--- docs/assets/js/videoControls/videoControls.js
+++ docs/assets/js/videoControls/videoControls.js
@@ -33,7 +33,7 @@ var lang,
   changeButtonType,
   canPlayVideo,
   resetPlayer,
-  link,focusNextElement,
+  link, focusNextElement,
   upperCaseFirst;
 
 var initialisevideo = function () {
@@ -108,26 +108,25 @@ var initialisevideo = function () {
   }, false);
 
   link.addEventListener('focusin', function (e) {
-    console.log('Link focus!');
     //focusNextElement();
   }, false);
 };
 
 var focusNextElement = function () {
-    //add all elements we want to include in our selection
-    var focussableElements = '.media-controls button';
-    if (document.activeElement && document.activeElement.form) {
-        var focussable = Array.prototype.filter.call(document.activeElement.form.querySelectorAll(focussableElements),
-        function (element) {
-            //check for visibility while always include the current activeElement
-            return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
-        });
-        var index = focussable.indexOf(document.activeElement);
-        if(index > -1) {
-           var nextElement = focussable[index + 1] || focussable[0];
-           nextElement.focus();
-        }
+  // Add tabindex to controls and keep correct order
+  var focussableElements = '.media-controls button';
+  if (document.activeElement && document.activeElement.form) {
+    var focussable = Array.prototype.filter.call(document.activeElement.form.querySelectorAll(focussableElements),
+      function (element) {
+        //check for visibility while always include the current activeElement
+        return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
+      });
+    var index = focussable.indexOf(document.activeElement);
+    if (index > -1) {
+      var nextElement = focussable[index + 1] || focussable[0];
+      nextElement.focus();
     }
+  }
 }
 
 var togglePlayPause = function () {
