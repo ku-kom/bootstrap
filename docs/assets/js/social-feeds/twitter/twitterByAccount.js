c3bcaf4590030a20c0f8ee0d4c7a4229ae85ab2f
nanna-dk <nanna@e-nanna.dk>
2020-07-06 14:30:43 +0200

    Twitter feed - don't render links if word is truncated

--- docs/assets/js/social-feeds/twitter/twitterByAccount.js
+++ docs/assets/js/social-feeds/twitter/twitterByAccount.js
@@ -46,7 +46,7 @@
             user: twitterAccount
           }
         }).done(function(feeds) {
-          console.log(feeds);
+          //console.log(feeds);
           wrapper.find(loading).addClass('hidden');
           $(feeds).each(function(i, e) {
             var tweetscreenname = e.user.name;
@@ -88,8 +88,9 @@
     }
 
     function renderLinks(data) {
-      //Add href to all links within tweets
-      data = data.replace(/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/g, function(url) {
+      //Add href to all links within tweets except if the word is truncated
+      var urlRegex = /(?!.*?\.\.\.)(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/g;
+      data = data.replace(urlRegex, function(url) {
         return '<a target="_blank" rel="noopener" href="' + url + '">' + url + '</a>';
       });
 
