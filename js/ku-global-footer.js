b48a5f5539c74b59a0eefea99b908f70a95cc4d0
nanna-dk <nanna@e-nanna.dk>
2020-08-06 14:25:52 +0200

    Experimenting with aria attributes

--- js/ku-global-footer.js
+++ js/ku-global-footer.js
@@ -19,9 +19,9 @@
       ev.preventDefault();
       $(el).next('ul').slideToggle();
       $(el).toggleClass('open');
-      console.log($(el).next('ul > li > a').prop('aria-expanded'));
+      console.log($(el).next('ul').find('li > a').prop('aria-expanded'));
       $(el).next('ul > li > a').prop('aria-expanded', function(i, val) {
-        console.log($(el).next('ul > li > a').prop('aria-expanded'));
+        console.log($(el).next('ul').find('li > a').prop('aria-expanded'));
         return val == 'false' ? true : false;
       });
     } else {
