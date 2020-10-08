b141af4d3829039e1d8aa3a9b9872ff09554e31c
nanna-dk <nanna@e-nanna.dk>
2020-10-08 09:38:26 +0200

    Added debounce function

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1858,6 +1858,27 @@ if (typeof jQuery === 'undefined') {
  * })(jQuery);
  *
  * ========================================================================*/
+function debounce(func, wait, immediate) {
+  // Use to delay function init.
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing. Usage, e.g. $(window).on('resize', debounce(myFunction, 250));
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
 function shareURL(dest) {
   // Usage, e.g.: onclick="shareURL('facebook')"
 
