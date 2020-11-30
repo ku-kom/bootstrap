527fba82fed88aa10d1c50ea4d58cc011b36db64
nanna-dk <nanna@e-nanna.dk>
2020-11-30 11:27:29 +0100

    Instagram fix - however undefined on VPN

--- docs/assets/js/social-feeds/instagram/instagramByAccount.js
+++ docs/assets/js/social-feeds/instagram/instagramByAccount.js
@@ -39,14 +39,24 @@
        }
        $container.empty();
        if (account) {
-         var $url = "https://www.instagram.com/" + encodeURIComponent($accountName) + "?__a=1";
-         $.ajax({
-             url: $url,
-             type: 'GET'
-           }).done(function(data) {
-             console.log(data);
+         var $url = "https://www.instagram.com/" + encodeURIComponent($accountName);
+         $.get($url, function(data) {
+             try {
+               data = data.split("window._sharedData = ")[1].split("<\/script>")[0];
+             } catch (e) {
+               console.log('Instagram Feed: It looks like the profile you are trying to fetch is age restricted. See https://github.com/jsanahuja/InstagramFeed/issues/26', 3);
+               return;
+             }
+             data = JSON.parse(data.substr(0, data.length - 1));
+             data = data.entry_data.ProfilePage;
+             if (typeof data === "undefined") {
+               console.log('Instagram Feed: It looks like YOUR network has been temporary banned because of too many requests. See https://github.com/jsanahuja/jquery.instagramFeed/issues/25', 4);
+               return;
+             }
+             data = data[0].graphql.user;
+             //console.log(data);
              $loading.hide();
-             var entry = data.graphql.user.edge_owner_to_timeline_media.edges;
+             var entry = data.edge_owner_to_timeline_media.edges;
              if (entry) {
                $.each(entry, function(i, v) {
                  var img = entry[i].node.thumbnail_src;
