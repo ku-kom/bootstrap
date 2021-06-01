a67cee1b9e6f018928e6ac49cc21e39d40f4b377
nanna-dk <nanna@e-nanna.dk>
2021-06-01 12:03:14 +0200

    Added scroll css

--- Gruntfile.js
+++ Gruntfile.js
@@ -279,7 +279,8 @@ module.exports = function (grunt) {
           'docs/assets/css/social-feeds/twitter.css',
           'docs/assets/css/ku-dk-frontpage/ku-dk-frontpage.css',
           'docs/assets/css/obvius-admin/admin.css',
-          'docs/assets/css/tinymce/tiny-overrides.css'
+          'docs/assets/css/tinymce/tiny-overrides.css',
+          'docs/assets/css/scroll-effects/scroll-effects.css',
           //'dist/css/faculties/*.css'
         ]
       },
