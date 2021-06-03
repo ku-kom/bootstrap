3dcc32111dc1c1fbcbaed4ab95971a07adea9bc8
nanna-dk <nanna@e-nanna.dk>
2021-06-03 15:54:49 +0200

    Finishing scroll script

--- docs/assets/js/scroll-progress/scroll-in-view.js
+++ docs/assets/js/scroll-progress/scroll-in-view.js
@@ -10,7 +10,6 @@
 function AnimateOnScroll(options) {
   'use strict';
 
-  //let _this = this;
   var defaultOptions = {
     element: '.js-scroll',
   }
@@ -20,32 +19,9 @@ function AnimateOnScroll(options) {
 
   const scrollElements = document.querySelectorAll(options.element);
 
-  const elementInView = (el, dividend = 1) => {
-    const elementTop = el.getBoundingClientRect().top;
-
-    return (
-      elementTop <=
-      (window.innerHeight || document.documentElement.clientHeight) / dividend
-    );
-  };
-
-  const elementOutofView = (el) => {
-    const elementTop = el.getBoundingClientRect().top;
-    const bounding = el.getBoundingClientRect();
-    return (
-      elementTop > (window.innerHeight || document.documentElement.clientHeight)
-    );
-  };
-
-  const displayScrollElement = (element) => {
-    element.classList.add('in-view');
-  };
-
-  const hideScrollElement = (element) => {
-    element.classList.remove('in-view');
-  };
-
   const isElementXPercentInViewport = function(element, percentVisible) {
+    // Check if an element is partially in view.
+    // percentVisible is how much of the element is visible before we return true.
     let
       rect = element.getBoundingClientRect(),
       windowHeight = (window.innerHeight || document.documentElement.clientHeight);
@@ -56,15 +32,17 @@ function AnimateOnScroll(options) {
     )
   };
 
+  const displayScrollElement = (element) => {
+    element.classList.add('in-view');
+  };
+
+  const hideScrollElement = (element) => {
+    element.classList.remove('in-view');
+  };
+
   const handleScrollAnimation = () => {
     scrollElements.forEach((el) => {
-      // if (elementInView(el, 1.25)) {
-      //   displayScrollElement(el);
-      // } else if (elementOutofView(el)) {
-      //   hideScrollElement(el)
-      // }
-
-      if (isElementXPercentInViewport(el, 75)) {
+      if (isElementXPercentInViewport(el, 4)) { // 4% visible
         displayScrollElement(el);
       } else {
         hideScrollElement(el)
