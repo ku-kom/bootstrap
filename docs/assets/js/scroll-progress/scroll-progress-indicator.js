6c41ab67be53b0c22d8af148c91ce1527579caa0
nanna-dk <nanna@e-nanna.dk>
2021-09-20 12:18:40 +0200

    Scroll progress indicator plugin

--- docs/assets/js/scroll-progress/scroll-progress-indicator.js
+++ docs/assets/js/scroll-progress/scroll-progress-indicator.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const appendProgressbar = () => {
     let target = document.querySelector('header:first-of-type');
     let bar = document.createElement('div');
-    bar.classList.add('progress');
+    bar.classList.add('progress', 'scroll-indicator');
     bar.innerHTML = '<div class="progress-bar" id="horizontal-scroll" role="progressbar" aria-valuetext="Scroll progress: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" tabindex="-1"></div>';
     let fragment = document.createDocumentFragment();
     fragment.appendChild(bar);
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
       let top = window.pageYOffset || document.documentElement.scrollTop;
       let el = document.querySelector('.progress');
       // Apply class to scroll progress bar after some scroll to make it visible...
-      el.classList.toggle('scroll-indicator', top > 20);
+      el.classList.toggle('in-view', top > 20);
     });
   }, {
     passive: true
