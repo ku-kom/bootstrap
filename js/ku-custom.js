d2710f0488fd304c111ae7daf0686c99e1e0d0b0
nanna-dk <nanna@e-nanna.dk>
2022-07-06 13:02:34 +0200

    Scroll to accordion fix

--- js/ku-custom.js
+++ js/ku-custom.js
@@ -201,16 +201,16 @@ function getCurrentScrollPosition() {
     }
   });
 
-  $(document).ajaxComplete(function(event) {
+  $(window).on('load', function() {
     // Open accordions based on the hash in the url
     var $accordion = window.location.hash.indexOf('collapse-') >= 0;
     if ($accordion) {
       var $acc = window.location.hash;
       $($acc).collapse('show');
 
-      $('html, body').animate({
-        scrollTop: $($acc).offset().top - 50
-      }, 800);
+      // $('html, body').animate({
+      //   scrollTop: $($acc).offset().top - 50
+      // }, 800);
     }
   });
 
