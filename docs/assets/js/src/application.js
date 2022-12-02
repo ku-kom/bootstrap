221444654c3a3c3b2fc8ffc2a4c723151e110251
nanna-dk <nanna@e-nanna.dk>
2022-12-02 14:43:11 +0100

    Remove affix sidebar in docs

--- docs/assets/js/src/application.js
+++ docs/assets/js/src/application.js
@@ -33,24 +33,24 @@
     })
 
     // // Sidenav affixing
-    setTimeout(function () {
-      var $sideBar = $('.bs-docs-sidebar')
+    // setTimeout(function () {
+    //   var $sideBar = $('.bs-docs-sidebar')
 
-      $sideBar.affix({
-        offset: {
-          top: function () {
-            var offsetTop      = $sideBar.offset().top
-            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-            var navOuterHeight = $('.bs-docs-nav').height()
+    //   $sideBar.affix({
+    //     offset: {
+    //       top: function () {
+    //         var offsetTop      = $sideBar.offset().top
+    //         var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+    //         var navOuterHeight = $('.bs-docs-nav').height()
 
-            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-          },
-          bottom: function () {
-            return (this.bottom = $('.bs-docs-footer').outerHeight(true))
-          }
-        }
-      })
-    }, 100)
+    //         return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+    //       },
+    //       bottom: function () {
+    //         return (this.bottom = $('.bs-docs-footer').outerHeight(true))
+    //       }
+    //     }
+    //   })
+    // }, 100)
 
     setTimeout(function () {
       $('.bs-top').affix()
