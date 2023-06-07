fcf004b825737311354fb4f286e8b92229d009ce
Nanna Ellegaard <nel@adm.ku.dk>
2023-06-07 09:23:59 +0200

    Add scrollspy

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,7 +121,7 @@ module.exports = function (grunt) {
           'js/modal.js',
           'js/tooltip.js',
           'js/popover.js',
-          //'js/scrollspy.js',
+          'js/scrollspy.js',
           'js/tab.js',
           //'js/affix.js',
           'js/ku-custom.js'
